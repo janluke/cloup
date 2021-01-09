e6beec94e81103bfb2bc6fdb991978026c23fc0a
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2021-01-09 20:17:12 +0100

    Docs: use sphinx_book_theme

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.autodoc.typehints',
     'sphinx_tabs.tabs',
-    'sphinx_copybutton',
+    'sphinx_copybutton',  # adds a copy button to code blocks
 ]
 autoapi_type = 'python'
 autoapi_dirs = [os.path.join(PROJ_DIR, 'cloup')]
@@ -109,40 +109,16 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_material'
-# Material theme options (see theme.conf for more information)
+html_title = "Cloup v{version}".format(version=version)
+html_theme = "sphinx_book_theme"
 html_theme_options = {
-    # Set the name of the project to appear in the navigation.
-    # 'nav_title': 'cloup docs',
-    "heroes": {
-        "index": "Option groups and subcommand sections for click.",
-    },
-    # Set you GA account ID to enable tracking
-    # 'google_analytics_account': 'UA-XXXXX',
-
-    # Specify a base_url used to generate sitemap.xml. If not
-    # specified, then no sitemap will be built.
-    # 'base_url': 'https://project.github.io/project',
-
-    # Set the color and the accent color
-    'color_primary': 'blue',
-    'color_accent': 'light-blue',
-
-    # Set the repo location to get a badge with stats
-    'repo_url': 'https://github.com/janluke/cloup/',
-    'repo_name': 'cloup',
-
-    # Visible levels of the global TOC; -1 means unlimited
-    'globaltoc_depth': 3,
-    # If False, expand all TOC entries
-    'globaltoc_collapse': True,
-    # If True, show hidden TOC entries
-    'globaltoc_includehidden': False,
+    "repository_url": "https://github.com/janluke/cloup",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "home_page_in_toc": False,
 }
-html_sidebars = {
-    "**": ["globaltoc.html", "searchbox.html"],
-}
-version_dropdown = True
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -151,6 +127,7 @@ html_static_path = ['_static']
 html_css_files = [
     'styles/copybutton-overrides.css',
     'styles/tabs-overrides.css',
+    'styles/theme-overrides.css',
 ]
 
 # -- Options for HTMLHelp output ---------------------------------------
