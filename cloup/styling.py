c21fe648807485a648b4595758b2acd37f9b0fd1
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2021-05-04 02:24:57 +0200

    Make HelpTheme a NamedTuple to work around MyPy issue

--- cloup/styling.py
+++ cloup/styling.py
@@ -1,55 +1,41 @@
 import dataclasses as dc
-from typing import Callable, Optional
+from typing import Callable, NamedTuple, Optional
 
 import click
 
 from cloup._util import identity
 
+#: A callable that takes a string and returns a styled version of it.
 IStyler = Callable[[str], str]
 
 
-@dc.dataclass(frozen=True)
-class Style:
-    fg: Optional[str] = None
-    bg: Optional[str] = None
-    bold: bool = False
-    dim: bool = False
-    underline: bool = False
-    blink: bool = False
-    reverse: bool = False
-    text_transform: Optional[IStyler] = None
-    _click_kwargs: dict = dc.field(init=False, default_factory=dict)
+class HelpTheme(NamedTuple):
+    """Stylesheet for various elements of the help page.
 
-    def __post_init__(self):
-        click_kwargs = dc.asdict(self)
-        click_kwargs.pop('text_transform')
-        click_kwargs.pop('_click_kwargs')
-        object.__setattr__(self, '_click_kwargs', {
-            key: (None if not value else value)
-            for key, value in click_kwargs.items()
-        })
-
-    def __call__(self, text: str) -> str:
-        if self.text_transform:
-            text = self.text_transform(text)
-        return click.style(text, **self._click_kwargs)
-
-
-@dc.dataclass(frozen=True)
-class HelpTheme:
+    **Implementation note:** this should have been a frozen ``dataclass``
+    but I had to use ``NamedTuple`` instead to work around a MyPy issue:
+    https://github.com/python/mypy/issues/5485. Don't rely on this class being
+    a ``NamedTuple`` because this will probably change in the future.
+    """
     #: Style of the command name.
     command: IStyler = identity
+
     #: Style of help section headings.
     heading: IStyler = identity
+
     #: Style of an option group constraint description.
     constraint: IStyler = identity
+
     #: Style of a help section descriptions.
     description: IStyler = identity
+
     #: Style of the first column of a definition list.
     col1: IStyler = identity
+
     #: Style of the second column of a definition list.
     col2: IStyler = identity
-    #: Style of the epilog
+
+    #: Style of the epilog.
     epilog: IStyler = identity
 
     def with_(
@@ -80,3 +66,30 @@ class HelpTheme:
             col1=Style(fg='bright_yellow'),
             epilog=Style(fg='bright_white'),
         )
+
+
+@dc.dataclass(frozen=True)
+class Style:
+    fg: Optional[str] = None
+    bg: Optional[str] = None
+    bold: bool = False
+    dim: bool = False
+    underline: bool = False
+    blink: bool = False
+    reverse: bool = False
+    text_transform: Optional[IStyler] = None
+    _click_kwargs: dict = dc.field(init=False, default_factory=dict)
+
+    def __post_init__(self):
+        click_kwargs = dc.asdict(self)
+        click_kwargs.pop('text_transform')
+        click_kwargs.pop('_click_kwargs')
+        object.__setattr__(self, '_click_kwargs', {
+            key: (None if not value else value)
+            for key, value in click_kwargs.items()
+        })
+
+    def __call__(self, text: str) -> str:
+        if self.text_transform:
+            text = self.text_transform(text)
+        return click.style(text, **self._click_kwargs)
