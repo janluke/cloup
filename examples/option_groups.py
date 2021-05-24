f9dc795932418526a894617c387f6f91c09e510f
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2021-05-24 12:51:31 +0200

    Examples: update examples/option_groups.py

--- examples/option_groups.py
+++ examples/option_groups.py
@@ -1,30 +1,13 @@
 """
 Example of options groups, "nested style" (recommended).
 
-The decorator @option_group() is overloaded with two signatures.
-Basically, you can specify the optional help string either:
-
-1) as keyword argument, after the options
-    @option_group(name: str,
-                  *options: Option,
-                  help: Optional[str] = None
-                  constraint: Optional[Constraint] = None)
-
-2) as 2nd positional argument, before the options
-
-    @option_group(name: str,
-                  help: str,
-                  *options: Option,
-                  constraint: Optional[Constraint] = None)
-
-ATTENTION: this is NOT and doesn't want to be a "meaningful" example!
-My goal here is to show you different ways of using the API in a compact way
-and give you a file that you can run to see how the help is printed out with
-different settings.
+NOTE: the goal of this example is to showcase Cloup's option groups and constraint API
+    in a compact way; the example is neither meaningful nor represents a good usage
+    of the library (in fact, you should always design your CLI so that it doesn't
+    need constraints).
 """
 from pprint import pprint
 
-import click
 from click import Choice
 
 import cloup
@@ -34,11 +17,11 @@ from cloup.constraints import (
 )
 
 
-@cloup.command(name='cloup', show_constraints=True, no_args_is_help=True)
-@click.argument('arg', required=False)
+@cloup.command(name='cloup', show_constraints=True)
+@cloup.argument('arg', required=False)   # alias of click.argument
 @option_group(
     'First group title',
-    "this is a very long description of the option group. I don't think this is "
+    "This is a very long description of the option group. I don't think this is "
     "needed very often; still, if you want to provide it, you can pass it as 2nd "
     "positional argument or as keyword argument 'help' after all options.",
     option('-o', '--one', help='a 1st cool option'),
@@ -55,12 +38,16 @@ from cloup.constraints import (
 )
 @option('--seven', help='an uncategorized option', type=Choice(['foo', 'bar']))
 @option('--eight', help='second uncategorized option')
+# Usage of @constraint
 @constraint(mutually_exclusive, ['one', 'two'])
-@constraint(If(Equal('one', '123'), then=RequireExactly(1)), ['seven', 'six'])
+@constraint(
+    If(Equal('one', '123'), then=RequireExactly(1)),
+    ['seven', 'six']
+)
 def main(**kwargs):
     """A test program for cloup."""
     pprint(kwargs, indent=3)
 
 
 if __name__ == '__main__':
-    main('--help'.split())
+    main()
