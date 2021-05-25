2e6dd229e5b17cfc2264ac1484e9a44c19215e49
Gianluca Gippetto <gianluca.gippetto@gmail.com>
2021-05-25 10:34:51 +0200

    Examples: remove module docstring in manim/render.py

--- examples/manim/render.py
+++ examples/manim/render.py
@@ -1,10 +1,3 @@
-"""Manim's default subcommand, render.
-
-Manim's render subcommand is accessed in the command-line interface via
-``manim``, but can be more explicitly accessed with ``manim render``. Here you
-can specify options, and arguments for the render command.
-
-"""
 from pathlib import Path
 from pprint import pprint
 
